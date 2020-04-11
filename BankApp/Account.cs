84dd8a72f8138ea249bbca274736b840010b69ca
Kal <info@kalacademy.org>
2020-04-11 14:03:04 -0700

    Add project files.

---
+++ BankApp/Account.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankApp
+{
+    /// <summary>
+    /// This class represents
+    /// a bank account, where you can 
+    /// deposit, withdraw...
+    /// </summary>
+    class Account
+    {
+        #region Properties
+        /// <summary>
+        /// Unique account number for the account
+        /// </summary>
+        public int AccountNumber { get; set; }
+        /// <summary>
+        /// Email address of the account holder
+        /// </summary>
+        public string EmailAddress { get; set; }
+        public string AccountName { get; set; }
+        public string AccountType { get; set; }
+        public decimal Balance { get; set; }
+        public DateTime CreatedDate { get; set; }
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Deposit money into your account
+        /// </summary>
+        /// <param name="amount">Amount to deposit</param>
+        public void Deposit(decimal amount)
+        {
+            //Balance = Balance + amount;
+            Balance += amount;
+        }
+
+        public decimal Withdraw(decimal amount)
+        {
+            Balance -= amount;
+            return Balance;
+        }
+
+        #endregion
+    }
+}
---
+++ BankApp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace BankApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Creating an instance of an account == object!
+            var myFirstAccount = new Account();
+            myFirstAccount.AccountNumber = 1234;
+            myFirstAccount.AccountName = "My checking";
+            myFirstAccount.AccountType = "Checking";
+            myFirstAccount.EmailAddress = "test@test.com";
+            myFirstAccount.Deposit(123456);
+
+            var mySecondAccount = new Account();
+            mySecondAccount.AccountNumber = 45678;
+            mySecondAccount.AccountName = "My savings";
+
+
+        }
+    }
+}
